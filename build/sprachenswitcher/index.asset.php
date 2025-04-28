a2d9ecf594edd094068df2a6ac02c89cd3358c76
saiminh <saiminh@gmail.com>
2025-04-28 16:33:51 +0200

    initial commit

---
+++ build/sprachenswitcher/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'd4cc3c3bc76e5c170815');
---
+++ src/sprachenswitcher/render.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ */
+
+?>
+<div <?php echo get_block_wrapper_attributes(); ?>>
+	<?php 
+  if ( function_exists( 'pll_the_languages' ) ) {
+    $langs_array = pll_the_languages(array(
+      'raw' => 1,
+      'hide_if_no_translation' => 0, // Don't hide links with no translations
+      'force_home' => 0,            // Don't force the home page
+      'echo' => 0                   // Don't echo, just return
+    ));
+    echo '<!-- Debug: ';
+    print_r($langs_array);
+    echo ' -->';
+  } ?>
+    <p class="lang-switcher">
+      <?php foreach ($langs_array as $lang) : ?>
+        <a class="lang_<?php esc_html_e( $lang['slug']); ?>" href="<?php esc_html_e( $lang['url'] ); ?>"><?php esc_html_e( $lang['name'] ); ?></a>
+      <?php endforeach; ?>
+    </p>
+</div>
